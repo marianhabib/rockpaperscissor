77d6e3253b4e0da3acf82f381518a4b3fdc2e988
Maria Khan <marianaveedhabib@gmail.com>
2023-04-05 20:10:54 +0500

    commiting changes before mergin

--- script.js
+++ script.js
@@ -2,20 +2,21 @@
 function computerchoice()
 {
    
-
     const options = ["Rock", "Paper", "Scissor"];
-
     const random = Math.floor(Math.random() * options.length);
-
+    if(options[random] == "Rock")
+    {document.getElementById("cmpchoice_img").src = "rock.png";}
+    else if (options[random] == "Paper"){document.getElementById("cmpchoice_img").src = "paper.png";}
+    else{document.getElementById("cmpchoice_img").src = "scissors.png";}
     return options[random];
 };
 
-
-function playerchoice()
+function playerchoice(choice,childDiv)
 {
-    let choice = prompt("please enter your coice");
-    return choice;
-};
+    childDiv.classList.add("chosen");
+    return choice.id;
+}
+
 function checkwinner(cchoice, pchoice)
 {
     cchoice = cchoice.toUpperCase();
